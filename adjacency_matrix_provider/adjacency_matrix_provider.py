b45311552833c1760ae83cab433cd914d8445462
nbayashi <naoya_nstyle@hotmail.co.jp>
2025-04-30 19:22:08 +0900

    add icon

--- adjacency_matrix_provider/adjacency_matrix_provider.py
+++ adjacency_matrix_provider/adjacency_matrix_provider.py
@@ -11,15 +11,6 @@
         copyright            : (C) 2025 by nbayashi
         email                : naoya_nstyle@hotmail.co.jp
  ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 """
 
 __author__ = 'nbayashi'
@@ -29,6 +20,8 @@ __copyright__ = '(C) 2025 by nbayashi'
 # This will get replaced with a git SHA1 when you do a git archive
 
 __revision__ = '$Format:%H$'
+from pathlib import Path
+from PyQt5.QtGui import QIcon
 
 from qgis.core import QgsProcessingProvider
 from .r_adjacency_matrix_algorithm import AdjacencyMatrixAlgorithm
@@ -45,22 +38,12 @@ from .lisa.lisa_knearneigh_algorithm import LISAKnearneighAlgorithm
 class AdjacencyMatrixProvider(QgsProcessingProvider):
 
     def __init__(self):
-        """
-        Default constructor.
-        """
         QgsProcessingProvider.__init__(self)
 
     def unload(self):
-        """
-        Unloads the provider. Any tear-down steps required by the provider
-        should be implemented here.
-        """
         pass
 
     def loadAlgorithms(self):
-        """
-        Loads all algorithms belonging to this provider.
-        """
         self.addAlgorithm(AdjacencyMatrixAlgorithm())
         self.addAlgorithm(KnearneighAlgorithm())
         self.addAlgorithm(DnearneighAlgorithm())
@@ -77,34 +60,15 @@ class AdjacencyMatrixProvider(QgsProcessingProvider):
 
 
     def id(self):
-        """
-        Returns the unique provider id, used for identifying the provider. This
-        string should be a unique, short, character only string, eg "qgis" or
-        "gdal". This string should not be localised.
-        """
         return 'rspatialstatistics'
 
     def name(self):
-        """
-        Returns the provider name, which is used to describe the provider
-        within the GUI.
-
-        This string should be short (e.g. "Lastools") and localised.
-        """
         return self.tr('R Spatial Statistics')
 
     def icon(self):
-        """
-        Should return a QIcon which is used for your provider inside
-        the Processing toolbox.
-        """
-        return QgsProcessingProvider.icon(self)
+        path = (Path(__file__).parent / "../r_spatial_stat_icon.png").resolve()
+        return QIcon(str(path))
+    
 
     def longName(self):
-        """
-        Returns the a longer version of the provider name, which can include
-        extra details such as version numbers. E.g. "Lastools LIDAR tools
-        (version 2.2.1)". This string should be localised. The default
-        implementation returns the same string as name().
-        """
         return self.name()
